d0320e9b3b3b8ddae66dabc8563717d2ac780b3c
Hércules <66522811+HerculesAlvesdaSilva@users.noreply.github.com>
2023-05-16 12:59:52 -0300

    inserindo php
    Começando a colocar código em php

---
+++ exercicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets\css\estilo.css" />
+    <title>Curso PHP</title>
+  </head>
+  <body>
+    <header class="cabecalho">
+      <h1>Curso PHP</h1>
+      <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+      <div class="conteudo">
+      </div>  
+    </main>
+    <footer class="rodape">Aluno © <?= date('Y'); ?></footer>
+  </body>
+</html>
